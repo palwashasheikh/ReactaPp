a938ccc7105905346de16bd423d6e03f3957bc22
Muhammad Shiraz <muhammadshiraz996@gmail.com>
2024-09-17 15:15:45 +0300

    Footer is ready

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,26 +2,25 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#295983] py-12 text-white">
+    <footer className="bg-blueish-80 py-12 text-white">
       <div className="max-w-6xl mx-auto px-4 grid grid-cols-1 sm:grid-cols-4 gap-8">
         {/* Krachtdak section with logo */}
         <div>
-          <div className="flex items-center">
-            <h3 className="text-2xl font-bold mb-1">Krachtdak</h3>
+          <div className="flex items-center mb-5">            
             <img
-              src="/Vector.png"
+              src="/logo.svg"
               alt="Krachtdak Logo"
-              className="ml-1 w-7 h-7 bg-white p-1"
+              className=""
             />
           </div>
-          <p className="mt-2">help@krachtak.com</p>
-          <p >+1234 456 678 89</p>
+          <p className="mt-2 flex justify-start items-center mb-3"><img src="/help.png" alt="Help" className="mr-1.5" /> help@krachtak.com</p>
+          <p className="mt-2 flex justify-start items-center"><img src="/phone.png" alt="Phone" className="mr-1.5" /> +1234 456 678 89</p>
         </div>
 
         {/* Links section */}
         <div>
-          <h3 className="font-semibold mb-4">Links</h3>
-          <ul>
+          <h3 className="font-semibold mb-4 text-2xl">Links</h3>
+          <ul className="flex flex-col space-y-3">
             <li><a href="/" className="hover:underline">Over Krachtdak</a></li>
             <li><a href="/" className="hover:underline">Algemene Voorwaarden</a></li>
             <li><a href="/" className="hover:underline">Ziektek</a></li>
@@ -34,8 +33,8 @@ const Footer = () => {
 
         {/* Legal section */}
         <div>
-          <h3 className="font-semibold mb-4">Legal</h3>
-          <ul>
+          <h3 className="font-semibold mb-4 text-2xl">Legal</h3>
+          <ul className="flex flex-col space-y-3">
             <li><a href="/" className="hover:underline">Terms Of Use</a></li>
             <li><a href="/" className="hover:underline">Privacy Policy</a></li>
             <li><a href="/" className="hover:underline">Cookie Policy</a></li>
@@ -44,8 +43,8 @@ const Footer = () => {
 
         {/* Knowledge Base section */}
         <div>
-          <h3 className="font-semibold mb-4">Knowledge Base</h3>
-          <ul>
+          <h3 className="font-semibold mb-4 text-2xl">Knowledge Base</h3>
+          <ul className="flex flex-col space-y-3">
             <li><a href="/" className="hover:underline">What Is A Good Solar Panel?</a></li>
             <li><a href="/" className="hover:underline">What Is A Good Brand Of Solar Panels?</a></li>
             <li><a href="/" className="hover:underline">Type Of Inverters</a></li>
@@ -54,7 +53,7 @@ const Footer = () => {
       </div>
 
       {/* Footer Bottom */}
-      <div className="mt-8 text-center text-sm">
+      <div className="mt-8 text-center text-sm border-t border-white/30 pt-10 w-2/3 mx-auto">
         <p>Copyright © 2024 Krachtdak. All Rights Reserved</p>
       </div>
     </footer>
