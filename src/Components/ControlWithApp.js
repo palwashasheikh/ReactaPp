7d4361d7087ac60dfdb692a425386d9f94b6df61
palwashasheikh <58906553+palwashasheikh@users.noreply.github.com>
2024-09-21 14:33:06 +0400

    changes done

--- src/Components/ControlWithApp.js
+++ src/Components/ControlWithApp.js
@@ -48,40 +48,45 @@ const ControlWithApp = () => {
           {/* Mobile View */}
           <div className="flex flex-col md:hidden items-center">
             {/* Small White Box Positioned Directly on Top of the Image */}
-            <div className="relative bg-white p-4 rounded-lg shadow-lg mb-2 ">
-              <p className="text-base font-semibold text-gray-600">Vergrendel de kabel via de app</p>
+            <div className="relative bg-white p-4 rounded-lg shadow-lg mb-5 " style={{ height: '60px', zIndex : '1' }}>
+
+            <p className="text-base font-semibold text-gray-600 top-3 " >Vergrendel de kabel via de app</p>
+          </div>  
               {/* Small Yellow Line from White Box to Image */}
-              <div className="absolute left-1/2 transform -translate-x-1/2 bottom-[-10px] -top-[75px] right-[100px]'">
-                <img src='/lineleft.png' alt="Yellow line" className="w-full " />
+              <div className="absolute left-1/2 transform -translate-x-[10%] bottom-[4rem] top-[45px]">
+                <img src='/lineleft.png' alt="Yellow line" className="w-full " style={{ zIndex: '1' }} />
               </div>
-            </div>
+            
 
             {/* Mobile Image */}
-            <div className="relative z-10">
+            <div className="relative z-10 mt-4">
               <img src="/iPhoneBG2-1-969x1536 1 (1).png" alt="App on mobile" className="max-w-xs mx-auto" />
             </div>
 
             {/* Blue and White Boxes Below the Image */}
             <div className="relative flex flex-col space-y-4 w-[355px]">
               {/* Blue Box */}
-              <div className="relative bg-[#06627C] text-white p-5 rounded-xl w-full mb-4 overflow-hidden">
-                <p className="text-base font-medium">
+              <div className="relative bg-[#06627C] text-white p-5 rounded-xl w-full mb-4 " style={{ height: 'auto' ,zIndex:'1'}}>
+                <p className="text-base font-medium z-10  ">
                   Plan je laadsessie in en heb volledige controle over wanneer - en hoelang - je wilt opladen. Pas de instellingen voor het laden van je elektrische auto aan met onze handige planningsfunctie. Stel verschillende laadtijden in voor doordeweekse dagen en het weekend zodat dit past bij jouw routine, optimaliseer vervolgens voor minder kosten en meer gemak.
                 </p>
-                {/* Line Connecting Blue Box and Mobile Image */}
-                <div className="absolute left-1/2 transform -translate-x-1/2 top-[-10px]">
-                  <img src='/lineleft.png' alt="Blue line" className="w-full" />
                 </div>
-              </div>
+                {/* Line Connecting Blue Box and Mobile Image */}
+                <div className="absolute left-[30%] transform -translate-x-[50%] top-[-350px] bottom-[-40px]">
+                  <img src='/lineleft.png' alt="Blue line" className="w-full " style={{ zIndex:'1' }} />
+                </div>
+              
 
               {/* White Box Below */}
-              <div className="relative bg-white p-4 rounded-lg shadow-lg overflow-hidden">
-                <p className="text-base font-semibold text-gray-600">Overzicht over je laadgeschiedenis via de app</p>
+              
+                <div className="relative bg-white p-4 rounded-lg shadow-lg border border-gray-300 md:border-solid md:border-white-400 md:shadow-xl" style={{  zIndex:'1' }}>
+  <p className="text-base font-semibold text-gray-600">Overzicht over je laadgeschiedenis via de app</p>
+</div>
                 {/* Line Connecting White Box and Mobile Image */}
-                <div className="absolute left-[45] bottom-3 ">
-                  <img src='/lineright.png' alt="White line" className="w-full h-auto" />
+                <div className="absolute left-[80%] transform -translate-x-1/2 top-[-291px] ">
+                  <img src='/lineright.png' alt="White line" className="w-full  h-[1200px]" style={{ zIndex: '1' }} />
                 </div>
-              </div>
+             
             </div>
           </div>
         </div>
